f0e6e64d2959d2f701759a6a3ed2f54f8c040ef3
George Lim <lim.george@me.com>
2021-03-13 22:56:17 -0500

    Add website

--- README.md
+++ README.md
@@ -1,35 +1,7 @@
-# Basic Template
+# george-lim.github.io
 
-[![releases](https://img.shields.io/github/v/release/george-lim/basic-template)](https://github.com/george-lim/basic-template/releases)
-[![ci](https://github.com/george-lim/basic-template/workflows/CI/badge.svg)](https://github.com/george-lim/basic-template/actions)
-[![license](https://img.shields.io/github/license/george-lim/basic-template)](https://github.com/george-lim/basic-template/blob/main/LICENSE)
+[![releases](https://img.shields.io/github/v/release/george-lim/george-lim.github.io)](https://github.com/george-lim/george-lim.github.io/releases)
+[![ci](https://github.com/george-lim/george-lim.github.io/workflows/CI/badge.svg)](https://github.com/george-lim/george-lim.github.io/actions)
+[![license](https://img.shields.io/github/license/george-lim/george-lim.github.io)](https://github.com/george-lim/george-lim.github.io/blob/main/LICENSE)
 
-## [Usage](#usage) | [Features](#features) | [Examples](#examples) | [CI/CD](#cicd)
-
-Basic Template is a template repository that provides basic CI/CD workflows.
-
-## Usage
-
-Choose `george-lim/basic-template` as the template when creating a new repository.
-
-## Features
-
-Basic Template provides a `README.md`, `LICENSE` and two workflows for GitHub Actions.
-
-## Examples
-
-There are no examples to provide for Basic Template.
-
-## CI/CD
-
-### Pipeline
-
-There are two workflows in this repository. Each workflow supports manual triggering.
-
-The `CI` workflow is automatically triggered whenever there is push activity in `main` or pull request activity towards `main`. It has one job:
-
-1. Lint the codebase with GitHub's [Super-Linter](https://github.com/github/super-linter).
-
-The `CD` workflow is automatically triggered whenever there is a tag pushed to the repository. It has one job:
-
-1. Create a GitHub release with the tag.
+George's website built with [app-store-web-port](https://github.com/george-lim/app-store-web-port).
---
+++ js/core.js
@@ -0,0 +1,110 @@
+$(function() {
+
+	$('html')
+	.removeClass('no-js')
+	.addClass('js');
+
+	// Device type detection
+	(function() {
+		var headerIndex = $('header.global').css('z-index');
+
+		switch (true) {
+			case (headerIndex == 250):
+			$('html').addClass('mobile');
+			case (headerIndex == 500):
+			$('html').addClass('tablet');
+			default:
+			$('html').addClass('desktop');
+		}
+	}());
+
+	// IE / Edge browser detection
+	if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+		$('html').addClass('IEedge');
+
+		// Fallback for object-fit: cover
+		$('img').each(function() {
+			var $img = $(this);
+			var imgUrl = $img.attr('src');
+
+			if (imgUrl) {
+				$img
+				.css('background-image', 'url(' + imgUrl + ')')
+				.attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+			}
+		});
+	}
+
+	// Desktop scripts
+	if ($('html').hasClass('desktop')) {
+    
+		// Update status bar time
+		(function updateSBTime() {
+			setTimeout(function() {
+				var date = new Date();
+				var hh = date.getHours();
+				var mm = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+				var period;
+
+				switch (true) {
+					case (hh == 0):
+					hh = 12;
+					case (hh <= 12):
+					period = 'AM';
+					break;
+					default:
+					hh -= 12;
+					period = 'PM';
+					break;
+				}
+
+				$('#sb-time').html(hh + ':' + mm + ' ' + period);
+				updateSBTime();
+			}, 250);
+		}());
+
+		// Scroll-assist
+		(function() {
+			var isScrolling = false;
+
+			$('.scroll-assist').on({mouseenter: function(e) {
+				e.preventDefault();
+
+				isScrolling = true;
+
+				var $scrollAssist = $(this);
+				var $bannerContainer;
+				var scrollDirection;
+
+				if ($scrollAssist.hasClass('left')) {
+					$bannerContainer = $scrollAssist.next();
+					scrollDirection = -1;
+				} else {
+					$bannerContainer = $scrollAssist.prev();
+					scrollDirection = 1;
+				}
+
+				var travelDistance = 5;
+				var maxTravelDistance = 20;
+
+				(function scroll() {
+					setTimeout(function() {
+						$bannerContainer.scrollLeft($bannerContainer.scrollLeft() + (travelDistance * scrollDirection));
+
+						if (travelDistance < maxTravelDistance) {
+							travelDistance += 0.2;
+						}
+
+						if (isScrolling) {
+							scroll();
+						}
+					}, 10);
+				}());
+			}, mouseleave: function(e) {
+				e.preventDefault();
+				isScrolling = false;
+				travelDistance = 5;
+			}});
+		}());
+	}
+});
